ca906436619556bddbb58f06c55025dee61d8172
Anthony Watts <anthonypwatts@gmail.com>
2026-04-09 11:47:19 +0100

    Fleshed out test rig and amended solution structure slightly.

--- ManualTestRig/Program.cs
+++ ManualTestRig/Program.cs
@@ -4,7 +4,12 @@ using Addressing.Validation;
 using Addressing.Spain;
 using Addressing.Utilities;
 
+// This is a test rig to validate the Addressing library's functionality.
+// It demonstrates how to use the library to validate addresses in different countries.
+// Check out BuildAddressingServiceProvider, which is where everything gets configured.
 var serviceProvider = BuildAddressingServiceProvider();
+
+// Get the address validator factory from the service provider
 var addressValidatorFactory = serviceProvider.GetRequiredService<IAddressValidatorFactory>();
 
 // Create a Spanish address
@@ -30,10 +35,54 @@ address = new Address(
     new PostalCode("SW1A 2AA", CountryCode.Parse("GB")),
     CountryCode.Parse("GB"));
 
-// Validate the UK address
+// Fail to validate the now UK address with the Spanish validator
+try
+{
+    countrySpecificAddressValidator.Validate(address);
+    Console.WriteLine("Unexpectedly, address is valid!");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Validation failed, as expected, with message: {ex.Message}");
+}
+
+// Validate the UK address with a UK validator
 countrySpecificAddressValidator = addressValidatorFactory.GetValidator(address.CountryCode);
 countrySpecificAddressValidator.Validate(address);
 Console.WriteLine("UK address is valid!");
+
+// Now make it an Australian address, which we don't have any validators defined for
+address = new Address(
+    "1 Infinite Loop",
+    null,
+    "Sydney",
+    null,
+    new PostalCode("2000", CountryCode.Parse("AU")),
+    CountryCode.Parse("AU"));
+
+// Prove it fails with the existing (Spanish) validator
+try
+{
+    countrySpecificAddressValidator.Validate(address);
+    Console.WriteLine("Unexpectedly, address is valid!");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Validation failed, as expected, with message: {ex.Message}");
+}
+
+// PRove it failes to get a validator for Australia
+try
+{
+    countrySpecificAddressValidator = addressValidatorFactory.GetValidator(address.CountryCode);
+    Console.WriteLine("Unexpectedly, address got itself a validator!");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Validator fetch failed, as expected, with message: {ex.Message}");
+}
+
+
 
 return 0;
 
